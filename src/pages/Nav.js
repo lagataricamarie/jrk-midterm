03a4f3eb96791e900d5722fc1d642442258e9d1c
lagataricamarie <147457628+lagataricamarie@users.noreply.github.com>
2024-04-19 22:09:33 +0800

    Update Nav.js

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -12,14 +12,15 @@ function Nav({Toggle}) {
     return (
     <nav className="navbar navbar-expand-sm navbar-black bg-transparent">
         <i className="navbar-brand bi bi-justify-left fs-4" onClick={Toggle}></i>
-        <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-            aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId"
+            aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
             <i className='bi bi-justify'></i>
         </button>
         <div className="collapse navbar-collapse" id="collapsibleNavId">
             <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
                 <li className="nav-item dropdown">
-                    <a className="bi bi-person fs-5 me-3 nav-link dropdown-toggle text-black" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a className="bi bi-person fs-5 me-3 nav-link dropdown-toggle text-black" href="#" id="dropdownId
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     </a>
                     <div className="dropdown-menu" aria-labelledby="dropdownId">
                         <a className="dropdown-item" href="#">Profile</a>
